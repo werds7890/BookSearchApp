1dd95099376e06a5d710447b683d025b72643372
werds7890 <hihyungul@naver.com>
2024-05-03 10:59:03 +0900

    ViewBinding, BottomNavigationView

--- app/src/main/java/com/example/booksearchapp/MainActivity.kt
+++ app/src/main/java/com/example/booksearchapp/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.booksearchapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.booksearchapp.databinding.ActivityMainBinding
+import com.example.booksearchapp.ui.view.FavoriteFragment
+import com.example.booksearchapp.ui.view.SearchFragment
+import com.example.booksearchapp.ui.view.SettingFragment
 
 class MainActivity : AppCompatActivity() {
+    private val binding : ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        setupBottomNavigationView()
+        if(savedInstanceState==null){
+            binding.bottomNavigationView.selectedItemId=R.id.fragment_search
+        }
+    }
+
+    private fun setupBottomNavigationView() {
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.fragment_search ->{
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SearchFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_favorite ->{
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, FavoriteFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_settings ->{
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SettingFragment())
+                        .commit()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
