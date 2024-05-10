9d1302dc0fe396ae83a9eec6d02c788fe74b8ab9
werds7890 <hihyungul@naver.com>
2024-05-10 14:17:50 +0900

    Retrofit, OkHttp, Moshi, Kapt

---
+++ app/src/main/java/com/example/booksearchapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.booksearchapp.util
+
+import com.example.booksearchapp.BuildConfig
+
+object Constants {
+    const val BASE_URL="https://dapi.kakao.com/"
+    const val API_KEY= BuildConfig.bookApiKey
+}
